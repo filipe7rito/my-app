09a31f16d1f7765cabb7237832c7d451413afde9
Filipe Rito <filipe@fidel.uk>
2022-06-01 11:22:45 +0100

    test branch name

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Amazing PR 18</p>
+        <p>Amazing PR 19</p>
         <a
           className="App-link"
           href="https://reactjs.org"
