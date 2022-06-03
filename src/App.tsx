d1c71a203fe6ec6aa2834f3f3000008576277cee
Filipe Rito <filipe@fidel.uk>
2022-06-03 16:57:56 +0100

    PR 25

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Amazing PR 24</p>
+        <p>Amazing PR 25</p>
         <a
           className="App-link"
           href="https://reactjs.org"
