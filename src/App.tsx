a9a2132e32f438cb30e019a090a87e31766cad6d
Filipe Rito <filipe@fidel.uk>
2022-08-24 16:46:10 +0100

    prd-30

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>DEPLOY AWS</p>
+        <p>30</p>
         <a
           className="App-link"
           href="https://reactjs.org"
